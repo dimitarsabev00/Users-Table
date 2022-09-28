120c214e6c6da25a4e0660fd4d437fe62384e7a7
Dimitar <wopz1337@gmail.com>
2022-09-28 18:43:58 +0300

    Added Users & Pagination Component

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,48 +2,26 @@ import React, { useEffect, useState } from "react";
 import "./Table.css";
 import styled from "@emotion/styled";
 import users_data from "./users_data.json";
+import Users from "./Users";
+import Pagination from "./Pagination";
 const Table = () => {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    setUsers(users_data);
-  }, []);
+  const [users, setUsers] = useState(users_data);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage, setUsersPerPage] = useState(10);
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = currentPage / usersPerPage;
+  const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
 
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div className="table" style={{ textAlign: "center" }}>
       <h1>Users Table</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => {
-            return (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.first_name}</td>
-                <td>{user.last_name}</td>
-                <td>{user.email}</td>
-                <td
-                  style={{
-                    display: "flex",
-                    gap: "1rem",
-                    justifyContent: "center",
-                  }}
-                >
-                  <button>Edit</button>
-                  <button>Delete</button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <Users users={currentUsers} setUsers={setUsers} />
+      <Pagination
+        usersPerPage={usersPerPage}
+        totalUsers={users.length}
+        paginate={paginate}
+      />
     </div>
   );
 };
