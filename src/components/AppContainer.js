696849f620143fa1b58fd16b37d918c4d5f13d75
Dimitar <wopz1337@gmail.com>
2022-09-28 20:36:58 +0300

    Fix Bug With Pagination

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -8,14 +8,34 @@ const AppContainer = () => {
   const [users, setUsers] = useState(users_data);
   const [currentPage, setCurrentPage] = useState(1);
   const [usersPerPage, setUsersPerPage] = useState(10);
+  const [searchPhrase, setSearchPhrase] = useState("");
   const indexOfLastUser = currentPage * usersPerPage;
-  const indexOfFirstUser = currentPage / usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
   const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const search = (e) => {
+    const matchedUser = users_data.filter((user) => {
+      return `${user.first_name} ${user.last_name} ${user.id} ${user.email}`
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
+    });
+
+    setUsers(matchedUser);
+    setSearchPhrase(e.target.value);
+  };
   return (
     <div className="table" style={{ textAlign: "center" }}>
       <h1>Users Table</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Search.."
+          value={searchPhrase}
+          onChange={search}
+        />
+      </div>
       <Users users={currentUsers} setUsers={setUsers} />
       <Pagination
         usersPerPage={usersPerPage}
