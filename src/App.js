0f95c59793b8e81e490c04ec856b16d8d2ae80a2
Dimitar <wopz1337@gmail.com>
2022-09-28 13:51:19 +0300

    Import Table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import styled from "@emotion/styled";
+import Table from "./components/Table";
 function App() {
-  const Heading = styled.h1`
-    text-align: center;
-    color: black;
-  `;
-  return <Heading>Hello From React!</Heading>;
+  return <Table />;
 }
 
 export default App;
