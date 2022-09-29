40cfb2c59468baf29b9195d4200e78709ef73eff
Dimitar <wopz1337@gmail.com>
2022-09-29 14:13:17 +0300

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import AppContainer from "./components/AppContainer";
+import AppContainer from "./pages/AppContainer/AppContainer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CreateUser from "./pages/CreateUser/CreateUser";
 function App() {
-  return <AppContainer />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppContainer />} />
+        <Route path="/create-user" element={<CreateUser />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
