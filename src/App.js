d60b07de0de995e88bd86957edef0deadb9b8cd7
Dimitar <wopz1337@gmail.com>
2022-09-29 16:20:32 +0300

    Rename Imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import AppContainer from "./pages/AppContainer/AppContainer";
+import HomePage from "./pages/HomePage/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateUser from "./pages/CreateUser/CreateUser";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AppContainer />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/create-user" element={<CreateUser />} />
       </Routes>
     </Router>
