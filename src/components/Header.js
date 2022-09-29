36f76e5e41f0794aff6c4f6427303432b26b5aa4
Dimitar <wopz1337@gmail.com>
2022-09-29 16:16:56 +0300

    Added Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+const Header = () => {
+  const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
+  const Button = styled.button`
+    width: 100px;
+    height: 30px;
+    font-family: inherit;
+    font-weight: 600;
+    font-size: 16px;
+    margin: 0 10px;
+    border-radius: 6px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    background: transparent;
+    border-color: #eee;
+  `;
+
+  return (
+    <Container>
+      <h1>Users Table</h1>
+
+      <Button>
+        <Link
+          style={{ textDecoration: "none", color: "black" }}
+          to="/create-user"
+        >
+          Create User
+        </Link>
+      </Button>
+    </Container>
+  );
+};
+
+export default Header;
