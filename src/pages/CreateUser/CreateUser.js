6171e4deca7f1a6ae57eb7f3c83d6fe7665c2870
Dimitar <wopz1337@gmail.com>
2022-09-30 08:44:57 +0300

    Fix Bug With Styles

--- src/pages/CreateUser/CreateUser.js
+++ src/pages/CreateUser/CreateUser.js
@@ -3,6 +3,49 @@ import React, { useState } from "react";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../../configs/firebase";
 import { useNavigate } from "react-router-dom";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Container = styled.div`
+  margin: 0 auto;
+  width: 60%;
+  margin-top: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  border: 2px solid rgba(0, 0, 0, 0);
+  border-radius: 10px;
+  outline: none;
+  background-color: rgba(230, 230, 230, 0.6);
+  padding: 0.5rem 1rem;
+  font-size: 1.1rem;
+  margin-bottom: 22px;
+  transition: 0.3s;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 0.5rem 1rem;
+  background-color: #2ecc71;
+  border-radius: 10px;
+
+  color: #fff;
+  font-size: 1.1rem;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: 0.3s;
+  &:hover {
+    background-color: #27ae60;
+  }
+`;
+const Heading = styled.h2``;
 const CreateUser = () => {
   const [email, setEmail] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -19,47 +62,6 @@ const CreateUser = () => {
     });
     navigate("/");
   };
-  const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  `;
-  const Container = styled.div`
-    margin: 0 auto;
-    width: 60%;
-    margin-top: 3rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  `;
-
-  const Input = styled.input`
-    border: 2px solid rgba(0, 0, 0, 0);
-    border-radius: 10px;
-    outline: none;
-    background-color: rgba(230, 230, 230, 0.6);
-    padding: 0.5rem 1rem;
-    font-size: 1.1rem;
-    margin-bottom: 22px;
-    transition: 0.3s;
-  `;
-  const Button = styled.button`
-    width: 100%;
-    padding: 0.5rem 1rem;
-    background-color: #2ecc71;
-    border-radius: 10px;
-
-    color: #fff;
-    font-size: 1.1rem;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    transition: 0.3s;
-    &:hover {
-      background-color: #27ae60;
-    }
-  `;
-  const Heading = styled.h2``;
 
   return (
     <Container>
