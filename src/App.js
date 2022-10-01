2fae1a24d3d1f4ac5f59adf2a5cf1bb35eb57e27
Dimitar <wopz1337@gmail.com>
2022-10-02 02:34:43 +0300

    Added Edit Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import HomePage from "./pages/HomePage/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateUser from "./pages/CreateUser/CreateUser";
+import EditUser from "./pages/EditUser/EditUser";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create-user" element={<CreateUser />} />
+        <Route path="/edit-user/:id" element={<EditUser />} />
       </Routes>
     </Router>
   );
