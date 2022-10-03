9cf46c8233cdaa99d2077b046d205af1dcea7f90
Dimitar <wopz1337@gmail.com>
2022-10-03 15:35:43 +0300

    Move Context Provider In App Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import HomePage from "./pages/HomePage/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateUser from "./pages/CreateUser/CreateUser";
+import { AppContextProvider } from "./context/AppContextProvider";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create-user" element={<CreateUser />} />
-      </Routes>
-    </Router>
+    <AppContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create-user" element={<CreateUser />} />
+        </Routes>
+      </Router>
+    </AppContextProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AppContextProvider } from "./context/AppContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AppContextProvider>
+  <>
     <App />
-  </AppContextProvider>
+  </>
 );
